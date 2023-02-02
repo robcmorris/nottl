9f78aa524b6ebc2ebe2d91f714778a5ed8a24248
Rob C Morris <99232975+robcmorris@users.noreply.github.com>
2023-02-01 21:32:29 -0500

    now a dictionary app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Home Page</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, velit cupiditate! Error quis aut, laboriosam exercitationem animi quibusdam nisi rem ipsa, totam modi velit consectetur? Doloremque labore perspiciatis eveniet veniam?</p>
+<script lang="ts">
+    interface Definition {
+        word: string;
+        phonetic: string;
+        meanings: [{
+            partOfSpeech: string;
+            definitions: [{
+                definition: string;
+            }]
+        }]
+        sourceUrls: string;
+        }
+
+    let searchTerm = '';
+    let definitions: Definition[] = [];
+  
+    const handleSubmit = async (event: SubmitEvent) => {
+      event.preventDefault();
+      const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`);
+      const data = await response.json();
+      definitions = data;
+    };
+  
+    const handleInput = (event: InputEvent) => {
+        if (event.target) {
+            searchTerm = (event.target as HTMLInputElement).value;
+        }
+    };
+  </script>
+  
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="text" bind:value={searchTerm} on:input={handleInput} />
+    <button type="submit">Search</button>
+  </form>
+
+
+  {#if definitions.length}
+    {#each definitions as definition}
+         <p>{definition.word}</p>
+         <p>{definition.phonetic}</p>
+         {#each definition.meanings as meanings}
+            <p>{meanings.partOfSpeech}</p>
+            {#each meanings.definitions as definitions}
+                 <p>{definitions.definition}</p>
+            {/each}
+         {/each}
+         <a href="{definition.sourceUrls}">Source</a>
+    {/each}
+  {:else}
+     <p>Try searching.</p>
+  {/if}
+  
