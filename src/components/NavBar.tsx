063adf7af6eec887b3622ca5b0ae4e06064da73d
Rob C Morris <99232975+robcmorris@users.noreply.github.com>
2023-02-06 22:19:02 -0500

    adding some pages and navigation

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import Link from "next/link";
+
+interface NavLink {
+  to: string;
+  children: React.ReactNode;
+}
+
+function NavLink({ to, children }: NavLink) {
+  return (
+    <Link href={to} className={`mx-4 font-medium`}>
+      {children}
+    </Link>
+  );
+}
+
+interface MobileNav {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+function MobileNav({ open, setOpen }: MobileNav) {
+  return (
+    <div
+      className={`absolute top-0 left-0 h-screen w-screen transform bg-white ${
+        open ? "-translate-x-0" : "-translate-x-full"
+      } drop-shadow-md filter transition-transform duration-300 ease-in-out `}
+    >
+      <div className="flex h-20 items-center justify-center bg-white drop-shadow-md filter">
+        {" "}
+        {/*logo container*/}
+        <Link className="text-xl font-semibold" href="/">
+          Nottl.io
+        </Link>
+      </div>
+      <div className="ml-4 flex flex-col">
+        <Link
+          className="my-4 text-xl font-medium"
+          href="/about"
+          onClick={() =>
+            setTimeout(() => {
+              console.log(open);
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          About
+        </Link>
+        <Link
+          className="my-4 text-xl font-medium"
+          href="/services"
+          onClick={() =>
+            setTimeout(() => {
+              console.log(open);
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          Services
+        </Link>
+        <Link
+          className="my-4 text-xl font-medium"
+          href="/projects"
+          onClick={() =>
+            setTimeout(() => {
+              console.log(open);
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          Projects
+        </Link>
+        <Link
+          className="my-4 text-xl font-medium"
+          href="/contact"
+          onClick={() =>
+            setTimeout(() => {
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="flex h-20 items-center bg-white px-4 py-4 drop-shadow-md filter">
+      <MobileNav open={open} setOpen={setOpen} />
+      <div className="flex w-3/12 items-center">
+        <Link className="text-2xl font-semibold" href="/">
+          Nottl.io
+        </Link>
+      </div>
+      <div className="flex w-9/12 items-center justify-end">
+        <div
+          className="relative z-50 flex h-8 w-8 flex-col items-center justify-between md:hidden"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {/* hamburger button */}
+          <span
+            className={`h-1 w-full transform rounded-lg bg-black transition duration-300 ease-in-out ${
+              open ? "translate-y-3.5 rotate-45" : ""
+            }`}
+          />
+          <span
+            className={`h-1 w-full rounded-lg bg-black transition-all duration-300 ease-in-out ${
+              open ? "w-0" : "w-full"
+            }`}
+          />
+          <span
+            className={`h-1 w-full transform rounded-lg bg-black transition duration-300 ease-in-out ${
+              open ? "-translate-y-3.5 -rotate-45" : ""
+            }`}
+          />
+        </div>
+
+        <div className="hidden md:flex">
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import Head from "next/head";
+import SEO from "../components/SEO";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "../utils/api";
@@ -9,21 +9,41 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>Nottl.io</title>
-        <meta name="description" content="Home of Nottl.io" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <SEO
+        title="Nottl.io - Home Page"
+        url="https://nottl.io"
+        description="Home Page for Nottl.io"
+      />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Nottl<span className="text-[hsl(280,100%,70%)]">.</span>io
-          </h1>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
+          <div className="py-12 sm:py-8 md:py-12 lg:py-14 xl:py-12 2xl:py-28">
+            <div className="mx-auto max-w-[800px] xl:max-w-7xl">
+              <div className="lg:px-8">
+                <div className="flex flex-col items-center">
+                  <div className="max-w-md px-4 sm:max-w-2xl sm:px-6 md:max-w-3xl lg:max-w-4xl lg:px-0 xl:max-w-5xl 2xl:max-w-6xl">
+                    <div className="flex w-full flex-col items-center gap-4">
+                      <div className="flex flex-col items-start justify-between">
+                        <div className="relative mt-4 flex h-full flex-col text-center xl:mt-8">
+                          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                            Nottl
+                            <span className="text-[hsl(280,100%,70%)]">.</span>
+                            io
+                          </h1>
+                          <div className="flex flex-col items-center gap-2">
+                            <p className="text-2xl text-white">
+                              {hello.data
+                                ? hello.data.greeting
+                                : "Loading tRPC query..."}
+                            </p>
+                            <AuthShowcase />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
@@ -48,10 +68,11 @@ const AuthShowcase: React.FC = () => {
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
+        type="button"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
-        {sessionData ? "Sign out" : "Sign in"}
+        {sessionData ? `Sign out` : "Sign in"}
       </button>
     </div>
   );
